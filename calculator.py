5ee731665fa0dcb644eabe940e61ee36520120d3
Julianna Repunte <jrepunte0209@gmail.com>
2021-12-01 23:41:19 +0000

    Added continuous ask feature

--- calculator.py
+++ calculator.py
@@ -4,34 +4,34 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 # Replace this with your code
 
-input_string = input("> ")
-tokens = input_string.split(' ')
-
-if tokens[0] == "q":
-    print("Thanks, see ya later!")
-    quit
-else:
-    if tokens[0] == "+":
-        answer = add(float(tokens[1]), float(tokens[2]))
-        print(answer)
-    if tokens[0] == "-":
-        answer = subtract(float(tokens[1]), float(tokens[2]))
-        print(answer)
-    if tokens[0] == "*":
-        answer = multiply(float(tokens[1]), float(tokens[2]))
-        print(answer)
-    if tokens[0] == "/":
-        answer = divide(float(tokens[1]), float(tokens[2]))
-        print(answer)
-    if tokens[0] == "square":
-        answer = square(float(tokens[1]))
-        print(answer)
-    if tokens[0] == "cube":
-        answer = cube(float(tokens[1]))
-        print(answer)
-    if tokens[0] == "pow":
-        answer = power(float(tokens[1]), float(tokens[2]))
-        print(answer)
-    if tokens[0] == "mod":
-        answer = mod(float(tokens[1]), float(tokens[2]))
-        print(answer)
+while True:
+    input_string = input("> ")
+    tokens = input_string.split(' ')
+    if tokens[0] == "q":
+        print("Thanks, see ya later!")
+        break
+    else:
+        if tokens[0] == "+":
+            answer = add(float(tokens[1]), float(tokens[2]))
+            print(answer)
+        if tokens[0] == "-":
+            answer = subtract(float(tokens[1]), float(tokens[2]))
+            print(answer)
+        if tokens[0] == "*":
+            answer = multiply(float(tokens[1]), float(tokens[2]))
+            print(answer)
+        if tokens[0] == "/":
+            answer = divide(float(tokens[1]), float(tokens[2]))
+            print(answer)
+        if tokens[0] == "square":
+            answer = square(float(tokens[1]))
+            print(answer)
+        if tokens[0] == "cube":
+            answer = cube(float(tokens[1]))
+            print(answer)
+        if tokens[0] == "pow":
+            answer = power(float(tokens[1]), float(tokens[2]))
+            print(answer)
+        if tokens[0] == "mod":
+            answer = mod(float(tokens[1]), float(tokens[2]))
+            print(answer)
