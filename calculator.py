c34723337a840b0a553c54843435ef561d68b990
Julianna Repunte <jrepunte0209@gmail.com>
2021-12-01 23:08:38 +0000

    Got User Input/Add Function Complete

--- calculator.py
+++ calculator.py
@@ -2,6 +2,16 @@
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
-
-
 # Replace this with your code
+
+input_string = input("> ")
+tokens = input_string.split(' ')
+print(f'{tokens}')
+
+if tokens[0] == "q":
+    print("Thanks, see ya later!")
+    quit
+else:
+    if tokens[0] == "+":
+        answer = add(int(tokens[1]), int(tokens[2]))
+        print(answer)
